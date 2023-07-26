b592dda1030bd03a179bf883bcbd7e021c40a523
host4bacteria <trishtinaldoctor@yahoo.com>
2023-07-26 18:16:40 -0400

    added questions + answers to js file

---
+++ script.js
@@ -0,0 +1,49 @@
+var questions = [
+{
+    question: "Commonly used data types DO NOT include:",
+    answers: [
+        { text: "strings", correct: false},
+        { text: "booleans", correct: false},
+        { text:"alerts", correct: true},
+        { text:"numbers", correct: false},
+    ]
+},
+{
+    question: "The condition in an if / else statement is enclosed with ____.",
+    answers: [
+        { text: "quotes", correct: false},
+        { text: "curly brackets", correct: true},
+        { text: "paranthesis", correct: false},
+        { text: "square brackets", correct: false},
+    ]
+},
+{
+    question: "Arrays in JavaScript can be used to store ____.",
+    answers: [
+        { text: "numbers andt strings", correct: false},
+        { text: "other arrays", correct: false},
+        { text: "booleans", correct: false},
+        { text: "all of the above", correct: true},
+    ]
+},
+{
+    question: "String values must be enclosed within ____ when beaing assigned to variables.",
+    answers: [
+        { text: "comnmas", correct: false},
+        { text: "curly brackets", correct: false},
+        { text: "quotes", correct: true},
+        { text: "paranthesis", correct: false},
+    ]
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+        { text: "JavaScript", correct: false},
+        { text: "terminal/bash", correct: false},
+        { text: "for loops", correct: false},
+        { text: "console.log", correct: true},
+    ]
+},
+
+
+]
